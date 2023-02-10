e57277bfdd0270e10b9f5cc41caac091db551597
SHAM <shamarknibbs@gmail.com>
2023-02-10 12:49:07 -0500

    added linear background to the start screen

--- GuessingGame/App.js
+++ GuessingGame/App.js
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,ImageBackground} from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import {LinearGradient} from 'expo-linear-gradient'
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
-
-    <StartGameScreen/></View>
+    <LinearGradient colors={['red','#ddb52f']}style={styles.rootScreen}>
+<ImageBackground source={require('./assets/images/dices.jpg')} resizeMode="cover"
+style={styles.rootScreen} imageStyle={styles.backgroundImage}>
+    <StartGameScreen/>
+    </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
 rootScreen:{
   flex:1,
-  backgroundColor:'#ddb52f'
+},
+backgroundImage:{
+  opacity:0.15
 }
 });
